8028db0c635c2afcd410dbcc790363762d01b5d6
Abhinav Ahuja <ahujaaa@amazon.com>
2025-09-30 06:27:49 +0530

    zzz

--- async.js
+++ async.js
@@ -1,4 +1,5 @@
 async function fetchProducts() {
+
   try {
     // after this line, our function will wait for the `fetch()` call to be settled
     // the `fetch()` call will either return a Response or throw an error
@@ -19,4 +20,84 @@ async function fetchProducts() {
 
 fetchProducts();
 console.log("4356");
+
+
+
+// async function fetchProducts() {
+//   try {
+//     // after this line, our function will wait for the `fetch()` call to be settled
+//     // the `fetch()` call will either return a Response or throw an error
+//     const response = await fetch(
+//       "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+//     );
+//     if (!response.ok) {
+//       throw new Error(`HTTP error: ${response.status}`);
+//     }
+//     // after this line, our function will wait for the `response.json()` call to be settled
+//     // the `response.json()` call will either return the parsed JSON object or throw an error
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error(`Could not get products: ${error}`);
+//   }
+// }
+
+// const promise = fetchProducts();
+// promise.then((data) => console.log(data[0]['name'])); // 
+
+
+// try {
+//   // using await outside an async function is only allowed in a module
+//   const response = await fetch(
+//     "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+//   );
+//   if (!response.ok) {
+//     throw new Error(`HTTP error: ${response.status}`);
+//   }
+    
+
+//   const data = await response.json();
+//   console.log(123);
+//   console.log(data[0].name);
+  
+// } catch (error) {
+//   console.error(`Could not get products: ${error}`);
+//   throw error;
+// }
+
+function log(msg) {
+  console.log(`${new Date().toISOString()} | ${msg}`);
+}
+
+try {
+  log("Before fetch");
+
+  const response = await fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+  );
+  console.log(123);
+  log("After fetch, before checking ok");
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+
+  log("Before response.json()");
+
+  const data = await response.json();
+
+  log("After response.json(), before console.log(123)");
+
+  
+
+  log("After console.log(123), before data[0].name");
+
+  console.log(data[0].name);
+
+  log("After console.log(data[0].name)");
+
+} catch (error) {
+  console.error(`Could not get products: ${error}`);
+  throw error;
+}
 
--- promises.js
+++ promises.js
@@ -8,9 +8,9 @@ const worldPromise = new Promise((resolve, reject) => {
 // helloPromise.then((msg) => console.log(msg))+' '+worldPromise.then((msg) => console.log(msg));
 
 
-helloPromise.then((msg1) => {
-  worldPromise.then((msg2) => console.log(msg1+' '+msg2))
-})
+// helloPromise.then((msg1) => {
+//   worldPromise.then((msg2) => console.log(msg1+' '+msg2))
+// })
 
 
 
@@ -28,10 +28,10 @@ helloPromise.then((msg1) => {
 
 // console.log("Started request…");
 
-console.log("-----------------------\n\n-----------------------");
+// console.log("-----------------------\n\n-----------------------");
 
 // const fetchPromise = fetch(
-//   "https://mdn.github.lala.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
 // );
 
 // fetchPromise.then((response) => {
@@ -41,11 +41,62 @@ console.log("-----------------------\n\n-----------------------");
 //     })
 // })
 
+// console.log('This should run first');
+
 // const mylist = "123";
 // fetchPromise
 //   .then((response) => response.json())
 //   .then((data) => console.log(data[0].name))
 //   .catch(() => console.log("There was some error!"))
 
+console.log("-----------------------\n\n-----------------------");
 
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function demo() {
+//   console.log("wait...");
+//   await sleep(5000);
+//   console.log("done!");
+// }
   
+// demo();
+
+// console.log("-----------------------\n\n-----------------------");
+
+// function waitForMessage() {
+//   return new Promise((resolve, reject) => {
+//     console.log("Starting async work...");
+
+//     // simulate async work (e.g. network call, DB query, etc.)
+//     setTimeout(() => {
+//       resolve("✅ Message arrived!");
+//     }, 2000); // resolves after 2 seconds
+//   });
+// }
+
+// // Usage
+// waitForMessage().then(msg => console.log(msg));
+
+// console.log("I can still do other stuff while waiting...");
+
+
+import { readFile } from "fs";
+
+function readFilePromise(path) {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+readFilePromise("./hello.txt")
+  .then(content => console.log("✅ File loaded:", content))
+  .catch(err => console.error("❌ Error:", err.message));
+
+console.log("This runs immediately, while the file is being read...");
+
